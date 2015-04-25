b2e85271a974afd45c0e519ec386eef1aedceccc
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-04-24 19:07:31 -0700

    Added GameManager
    It's better than having LevelHandler

---
+++ Ninja/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance;
+	public int points;
+
+	public static GameManager Instance {
+		get
+		{
+			if (instance == null)
+			{
+				instance = FindObjectOfType<GameManager>();
+				if (instance == null) {
+					GameObject obj = new GameObject();
+					instance = obj.AddComponent<GameManager>();
+				}
+			}
+			return instance;
+		}
+
+	}
+
+	public void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+		
+		// if there's no GameManager object, create one
+		if (instance == null)
+			instance = this as GameManager;
+		// if there's an existing GameManager, destroy it
+		else 
+			Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		points = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("p"))
+		{
+		    points++;
+			print (points);
+		}
+	}
+	
+}
