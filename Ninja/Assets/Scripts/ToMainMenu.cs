313d10fedc2d0c9b83f82a231df106e6c6408018
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-04-25 19:33:15 -0700

    Basic Tutorial Interface completed!
    Scrollable menu, instructions. Aww yeaa *^*

---
+++ Ninja/Assets/Scripts/ToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Script for ToMainMenu button
+
+public class ToMainMenu : MonoBehaviour {
+
+	public void GoBack ()
+	{
+		Application.LoadLevel ("SimpleMainMenu");
+	}
+}
