ffb8611f44f4a917a8c875ea47ab48a69fc98d00
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-05-09 13:14:26 -0700

    Created Smart Turret
    Detects a single ninja fine, but the turret doesn't rotate for some
    reason.

---
+++ Ninja/Assets/Scripts/RangeChecker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangeChecker : SmartTurretController {
+
+	void OnTriggerEnter2D (Collider2D col){
+		if (target == null && col.gameObject.tag == "ninja") {
+			target = col.transform;
+			Debug.Log ("Ninja in range");
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col){
+		target = null;
+		Debug.Log ("Ninja out of range.");
+	}
+}
---
+++ Ninja/Assets/Scripts/SmartTurretController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmartTurretController : MonoBehaviour {
+
+	// this is which Ninja the turret is gonna rotate towards
+	protected Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// why does our object rotate only when the code is placed outside of the if-statement??
+		if (target) {
+			Debug.Log ("Smart Turret found a Ninja!");
+
+			// LockOnPlayer();
+
+
+		}
+	
+	}
+
+	void LockOnPlayer ()
+	{
+
+		// Delays the turret rotation so it doesn't freak out whenever the player moves
+		Quaternion rotation = Quaternion.LookRotation (target.transform.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
+
+		transform.LookAt (target.transform);
+
+
+	}
+}
