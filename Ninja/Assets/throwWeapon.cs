819f372b5dfdcb12c657733f9af138ce7c4d09ff
ryosaito420 <ryocsaito@gmail.com>
2015-05-15 17:25:03 -0700

    throwing shurikens work :D
    dat glitch tho

--- Ninja/Assets/throwWeapon.cs
+++ Ninja/Assets/throwWeapon.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class throwWeapon : MonoBehaviour {
 
+	public GameObject shuriken;
+
 	// Use this for initialization
 	void Start () {
-	
+		gameObject.SetActive(true);
 	}
-	
+
+
+	void toss(){
+		Instantiate (shuriken, transform.position, transform.rotation);
+	}
 	// Update is called once per frame
 	void Update () {
-	
+
+		if( Input.GetKey("v")){
+			print( "fire");
+			toss ();
+
+		}
 	}
+
 }
