4821f42405de044f5ef424398307fa7abba29336
ryosaito420 <ryocsaito@gmail.com>
2015-05-15 18:43:17 -0700

    LEMONS :D
    JUMP And SHOOT MAN

--- Ninja/Assets/rotateShuriken.cs
+++ Ninja/Assets/rotateShuriken.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class rotateShuriken : MonoBehaviour {
 
 	public float rotateSpeed;
+	public float shurikenSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (shurikenSpeed, 0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate ( Vector3.forward * Time.deltaTime * -rotateSpeed);
+		Destroy (this.gameObject, 5);
 	}
+
+	void OnCollisionEnter2D(){
+		Destroy(this.gameObject);
+	}	
 }
