fc6cefbddab1c1acb4bfc18303df9e76c4a2dc24
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-05-18 18:46:32 -0700

    Turret rotation boundaries fixed
    Thanks Will!

--- Ninja/Assets/Scripts/SmartTurretController.cs
+++ Ninja/Assets/Scripts/SmartTurretController.cs
@@ -38,7 +38,8 @@ public class SmartTurretController : MonoBehaviour {
 	void idleRotate(){
 		transform.Rotate (Vector3.forward * Time.deltaTime * rotationSpeed, Space.World);
 
-		if (transform.rotation.eulerAngles == Vector3.forward * 90){
+		if (transform.rotation.eulerAngles.z > 359 || transform.rotation.eulerAngles.z < 180){
+			Debug.Log ("sdfs");
 			rotationSpeed = -rotationSpeed;
 		}
 	}
