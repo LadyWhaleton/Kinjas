8ed95a46c03a14f7128a5175d85adfecfcb6d5cc
ryosaito420 <ryocsaito@gmail.com>
2015-05-22 17:23:16 -0700

    trampoline base is working
    Need to add sprite to add animation

---
+++ Ninja/Assets/trampoline.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trampoline : MonoBehaviour {
+
+	public float trampolineForce;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "ninjaFeet") {
+			print ("steepped on trampoline");
+			col.transform.parent.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, trampolineForce));
+		}
+	}
+}
