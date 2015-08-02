5266d999e09716e7164db39a09bdb3b151fff990
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-08-01 17:29:30 -0700

    Added EventHandler, made Scroll a child of EH
    Basically does what ScrollController did (switch levels when all 4
    ninjas saved). Scroll game object is now placed under EventHandler game
    object

---
+++ Ninja/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour {
+	
+	public int ninjasSafe = 0;
+	private int winNumber = 4;
+	public bool goNextLevel = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ninjasSafe == winNumber) {
+			print("Level completed!");
+			ninjasSafe = 0;
+			goNextLevel = true;
+		}
+		
+		if (goNextLevel){
+			if(Application.loadedLevelName == "Basic Test Lvl" || Application.loadedLevelName == "Level1")
+				Application.LoadLevel("Level2");
+			else if(Application.loadedLevelName == "Level2")
+				Application.LoadLevel("level3");
+			else if(Application.loadedLevelName == "level3")
+				Application.LoadLevel("Level4");
+			else if (Application.loadedLevelName == "Level4")
+				Application.LoadLevel("TempEndScreen");
+			
+		}
+	}
+}
--- Ninja/Assets/Scripts/ResetScript.cs
+++ Ninja/Assets/Scripts/ResetScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ResetScript : MonoBehaviour {
 
 	private string[] konamiCode;
+	public ScrollController ScrollScript;
 
 	// Use this for initialization
 	void Start () {
