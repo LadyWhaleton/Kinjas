e84c77ba95ea42953e7261d5987fad2fea9d1d3c
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-10-01 19:35:24 -0700

    Added Object Destroyer, fixed level 3 bugs

---
+++ Ninja/Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroyer : MonoBehaviour {
+
+	public int NinjasKilled = 0;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "ninja") {
+			NinjasKilled++;
+		} 
+
+		Destroy (coll.gameObject);
+	}
+}
